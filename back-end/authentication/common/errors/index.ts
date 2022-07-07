896465fe1b4020abb30b928ba3c87b5306ed3f96
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-07 18:33:12 +0530

    user not found error code

--- back-end/authentication/common/errors/index.ts
+++ back-end/authentication/common/errors/index.ts
@@ -2,5 +2,6 @@ export * from "./generic-internal-error";
 export * from "./database-connection-error";
 export * from "./unhandled-path-error";
 export * from "./custom-error";
-export * from "./user-already-exists-error"
-export * from "./request-validation-failed-error"
+export * from "./user-already-exists-error";
+export * from "./request-validation-failed-error";
+export * from "./user-not-found-error";
