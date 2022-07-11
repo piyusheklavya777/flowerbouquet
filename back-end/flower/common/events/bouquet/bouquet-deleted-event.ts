24d4936a8658fd673e43dfae82ab88757c7ca461
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-11 15:18:12 +0530

    naming bug

--- back-end/flower/common/events/bouquet/bouquet-deleted-event.ts
+++ back-end/flower/common/events/bouquet/bouquet-deleted-event.ts
@@ -1,6 +1,6 @@
 import EventKey from '../event-keys';
 
-export interface FlowerDeletedEvent {
+export interface BouquetDeletedEvent {
   subject: EventKey.FLOWER_DELETED;
   data: {
     flowerId: string;
