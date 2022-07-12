15ead06db5872caa0dff200122676726792d464a
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-12 13:16:00 +0530

    remaming service

--- back-end/bouquet/deploy.sh
+++ back-end/bouquet/deploy.sh
@@ -1,4 +1,4 @@
-docker build -t eklav/fb-flower:latest .
-docker push eklav/fb-flower
-# docker run -p 3000:3000 eklav/fb-flower
+docker build -t eklav/fb-bouquet:latest .
+docker push eklav/fb-bouquet
+# docker run -p 3000:3000 eklav/fb-bouquet
 # kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
