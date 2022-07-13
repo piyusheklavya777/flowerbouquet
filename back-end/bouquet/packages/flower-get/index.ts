14b6f357eb51f034a70989ad747c4d63c2d4ee19
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-13 10:48:20 +0530

    get flower

---
+++ back-end/bouquet/packages/flower-get/index.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-param-reassign */
+import _ from 'lodash';
+import { Flower } from '../db/models/flower';
+import { logger } from '../../common';
+
+export async function getFlowers({ namePrefix }) {
+  if (namePrefix === "''" || _.isEmpty(namePrefix)) {
+    return _getAll();
+  }
+
+  let flowers;
+
+  if (namePrefix) {
+    flowers = await Flower.find({
+      name: {
+        $regex: `^${namePrefix}.*`,
+      },
+      isActive: true,
+    });
+  } else {
+    flowers = await Flower.find({ isActive: true });
+  }
+
+  const flowerObjects = _.map(flowers, (v) => v.toObject());
+
+  logger.info('flowerObjects', flowerObjects);
+
+  return flowerObjects;
+}
+
+async function _getAll() {
+  const flowers = await Flower.find({ isActive: true });
+  return flowers;
+}
