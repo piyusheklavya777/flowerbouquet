8f1383e5f9a0a6e5833b814df76622266a57508c
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-13 16:56:43 +0530

    renaming

---
+++ back-end/order/packages/bouquet/get/http-adapter.ts
@@ -0,0 +1,39 @@
+import _ from 'lodash';
+import Joi from 'joi';
+import { standardHttpResponseInterface } from '../../../common';
+import { validateAgainstJoiSchema } from '../../../common/utility-functions/validate-against-schema';
+import { handleErrorAndConvertToJSON } from '../../../common/utility-functions';
+import { getCurrentUser } from '../../../common/utility-functions/get-current-user';
+import { getBouquets } from '.';
+
+export async function getBouquetHttpHandler({ standardRequestObject }): Promise<standardHttpResponseInterface> {
+  try {
+    validateAgainstJoiSchema(standardRequestObject, bouquetGetHttpRequestSchema);
+
+    const { userId } = getCurrentUser(standardRequestObject.session);
+
+    const bouquetId = _.get(standardRequestObject, ['queryParameters', 'bouquetId']);
+    const namePrefix = _.get(standardRequestObject, ['queryStringParameters', 'namePrefix']);
+
+    const bouquets = await getBouquets({
+      bouquetId,
+      userId,
+      namePrefix,
+    });
+    return {
+      httpCode: 200,
+      body: bouquets,
+    };
+  } catch (e) {
+    return handleErrorAndConvertToJSON(e);
+  }
+}
+
+const bouquetGetHttpRequestSchema = Joi.object({
+  queryStringParameters: Joi.object({
+    namePrefix: Joi.string().min(1),
+  }),
+  queryParameters: Joi.object({
+    bouquetId: Joi.string().min(5),
+  }),
+}).unknown();
---
+++ back-end/order/packages/bouquet/get/index.ts
@@ -0,0 +1,67 @@
+/* eslint-disable no-param-reassign */
+import _ from 'lodash';
+import { Bouquet } from '../../db/models/bouquet';
+import { Flower } from '../../db/models/flower';
+import { BouquetNotFoundError, logger } from '../../../common';
+
+export async function getBouquets({ bouquetId, namePrefix, userId }) {
+  if (bouquetId) return _singleGet({ bouquetId, userId });
+
+  let bouquets;
+
+  if (namePrefix === "''" || _.isEmpty(namePrefix)) {
+    bouquets = await Bouquet.find({ isActive: true });
+  } else {
+    bouquets = await Bouquet.find({
+      name: {
+        $regex: `^${namePrefix}.*`,
+      },
+      isActive: true,
+    });
+  }
+
+  const bouquetObjects = _.map(bouquets, (v) => v.toObject());
+
+  logger.info('bouquetObjects', bouquetObjects);
+
+  const bouquetsWithoutProps = await Promise.all(
+    _.map(bouquetObjects, async (bouquet) => {
+      if (bouquet.creatorId === userId) {
+        bouquet.belongsToThisUser = true;
+      }
+      delete bouquet.creatorId;
+      return _populateBouquet(bouquet);
+    }),
+  );
+
+  return bouquetsWithoutProps;
+}
+
+async function _singleGet({ bouquetId, userId }) {
+  let bouquet;
+  try {
+    bouquet = (await Bouquet.findOne({ _id: bouquetId, isActive: true }))?.toObject();
+    if (!bouquet) {
+      throw new BouquetNotFoundError();
+    }
+  } catch (e) {
+    logger.error('error while fetching data from bouquet db', { e });
+    throw e;
+  }
+  logger.silly('bouquet', bouquet);
+  return _populateBouquet(bouquet);
+}
+
+async function _populateBouquet(bouquet) {
+  bouquet.flowers = await Promise.all(
+    _.map(bouquet.flowers, async (flower) => {
+      const flowerDetails = await Flower.findOne({ isActive: true, _id: flower.flowerId });
+      flower.name = flowerDetails?.name;
+      flower.price = flowerDetails?.price;
+      delete flower._id;
+      return flower;
+    }),
+  );
+
+  return bouquet;
+}
