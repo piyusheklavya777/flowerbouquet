3c1b2a5db21e6a4796e4c4e62e54cb75c4c1068b
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-13 16:58:32 +0530

    discount default

--- back-end/order/packages/bouquet-create.ts
+++ back-end/order/packages/bouquet-create.ts
@@ -1,7 +1,7 @@
 import { logger } from '../common';
 import { Bouquet } from './db/models/bouquet';
 
-const createBouquet = async ({ bouquetId, name, discount, flowers }) => {
+const createBouquet = async ({ bouquetId, name, discount = 0, flowers }) => {
   const bouquet = await Bouquet.build({ bouquetId, name, discount, flowers, isActive: true });
   await bouquet.save();
   logger.info(`${name} bouquet id: ${bouquetId} created successfully`, bouquet);
