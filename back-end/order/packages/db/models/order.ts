e8a44d2a85d5edf6e6a48b96cb4fa3d2de5e74d9
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-13 15:00:09 +0530

    orderSchema

---
+++ back-end/order/packages/db/models/order.ts
@@ -0,0 +1,115 @@
+/* eslint-disable func-names */
+/* eslint-disable no-param-reassign */
+import mongoose from 'mongoose';
+import { OrderStatus } from '../../../common';
+
+interface OrderAttributes {
+  isActive: boolean;
+  customerId: string;
+  status: string;
+  bouquets: {
+    bouquetId: string;
+    discount: number;
+    name: string;
+    flowers: {
+      flowerId: string;
+      name: string;
+      price: number;
+      quantity: number;
+    }[];
+  }[];
+}
+
+interface OrderDoc extends mongoose.Document {
+  isActive: boolean;
+  customerId: string;
+  status: string;
+  bouquets: {
+    bouquetId: string;
+    discount: number;
+    name: string;
+    flowers: {
+      flowerId: string;
+      name: string;
+      price: number;
+      quantity: number;
+    }[];
+  }[];
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttributes): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    customerId: {
+      type: String,
+      required: true,
+    },
+    customerName: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: OrderStatus,
+    },
+    bouquets: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        discount: {
+          type: Number,
+          default: 0,
+        },
+        bouquetId: {
+          type: String,
+          required: true,
+        },
+        flowers: [
+          {
+            flowerId: {
+              type: String,
+              required: true,
+            },
+            name: {
+              type: String,
+              required: true,
+            },
+            price: {
+              type: Number,
+              required: true,
+            },
+            quantity: {
+              type: Number,
+              required: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    toJSON: {
+      transform(_, returnDocument) {
+        returnDocument.id = returnDocument._id;
+        delete returnDocument._id;
+      },
+    },
+  },
+);
+
+orderSchema.set('versionKey', 'version');
+
+orderSchema.statics.build = (attrs: OrderAttributes) => new Order(attrs);
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Bouquet', orderSchema);
+
+export { Order };
