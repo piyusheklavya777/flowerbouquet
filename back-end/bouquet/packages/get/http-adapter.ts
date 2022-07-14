acf83cb21062493cad56b1c0390c28ec59f00c5e
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-14 19:58:08 +0530

    bouquet get to work without login

--- back-end/bouquet/packages/get/http-adapter.ts
+++ back-end/bouquet/packages/get/http-adapter.ts
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import Joi from 'joi';
-import { standardHttpResponseInterface } from '../../common';
+import { logger, standardHttpResponseInterface, UserNotLoggedInError } from '../../common';
 import { validateAgainstJoiSchema } from '../../common/utility-functions/validate-against-schema';
 import { handleErrorAndConvertToJSON } from '../../common/utility-functions';
 import { getCurrentUser } from '../../common/utility-functions/get-current-user';
@@ -9,15 +9,21 @@ import { getBouquets } from './index';
 export async function getBouquetHttpHandler({ standardRequestObject }): Promise<standardHttpResponseInterface> {
   try {
     validateAgainstJoiSchema(standardRequestObject, bouquetGetHttpRequestSchema);
-
-    const { userId } = getCurrentUser(standardRequestObject.session);
+    let userDetails;
+    try {
+      userDetails = getCurrentUser(standardRequestObject.session);
+    } catch (e) {
+      if (e instanceof UserNotLoggedInError) {
+        logger.info('User not logged in');
+      }
+    }
 
     const bouquetId = _.get(standardRequestObject, ['queryParameters', 'bouquetId']);
     const namePrefix = _.get(standardRequestObject, ['queryStringParameters', 'namePrefix']);
 
     const bouquets = await getBouquets({
       bouquetId,
-      userId,
+      userId: userDetails?.userId,
       namePrefix,
     });
     return {
