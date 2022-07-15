af887196cffa9120cd834f97653ff00ea529fe7e
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-07-15 10:46:49 +0530

    axios config

---
+++ front-end/hooks/use-request.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { useState } from 'react';
+import _ from 'lodash';
+
+export default ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async (props = {}) => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, { ...body, ...props });
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
+      return response.data;
+    } catch (e) {
+      console.log('lol', e);
+      const error = _.get(e, ['response', 'data']);
+      if (error) {
+        setErrors(
+          <div className="alert alert-danger">
+            <h4>{error.error}</h4>
+            <ul className="my-0">
+              {error.description}
+            </ul>
+          </div>
+        );
+      } else {
+        setErrors(
+          <div className="alert alert-danger">
+            <h4>Unknown error occurred. Check console.</h4>
+          </div>
+        );
+      }
+    }
+  };
+
+  return { doRequest, errors };
+};
